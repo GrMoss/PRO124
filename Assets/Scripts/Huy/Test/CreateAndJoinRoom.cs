007595a1ccc581c2021de5b7efa8d871e5242705
GrMoss <126696013+GrMoss@users.noreply.github.com>
2024-06-26 14:39:09 +0700

    .

--- Assets/Scripts/Huy/Test/CreateAndJoinRoom.cs
+++ Assets/Scripts/Huy/Test/CreateAndJoinRoom.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
@@ -21,8 +22,17 @@ public class CreateAndJoinRoom : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinRoom(joinInput.text);
     }
 
+    // Callback khi vào phòng thành công
     public override void OnJoinedRoom()
     {
         PhotonNetwork.LoadLevel("Game1");
+        Debug.Log("Đã vào phòng: " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    // Callback khi có người chơi khác vào phòng
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("Người chơi mới đã vào phòng: " + newPlayer.NickName);
+        
     }
 }
