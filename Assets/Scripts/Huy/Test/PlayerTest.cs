b6f0d80aaa922e1ca876b280ca179d14b8abcce4
GrMoss <126696013+GrMoss@users.noreply.github.com>
2024-07-04 12:23:26 +0700

    tách script photon BXH khỏi playerTest

--- Assets/Scripts/Huy/Test/PlayerTest.cs
+++ Assets/Scripts/Huy/Test/PlayerTest.cs
@@ -12,94 +12,95 @@ public class PlayerTest : MonoBehaviourPunCallbacks
     private Vector2 moveInput;
     private Rigidbody2D myRigidbody;
     private SpriteRenderer mySpriteRenderer;
-    private TMP_Text BXHText;
-    private TMP_Text BXHText2;
-    private TMP_Text scoreText;
-    public static int score;
-    public static bool showBXH = false;
+    //private TMP_Text BXHText;
+    //private TMP_Text BXHText2;
+    //private TMP_Text scoreText;
+    //public static int score;
+    //public static bool showBXH = false;
 
-    private static Dictionary<int, int> playerScores = new Dictionary<int, int>(); // Lưu trữ điểm số
-    PhotonView view;
+    //private static Dictionary<int, int> playerScores = new Dictionary<int, int>(); // Lưu trữ điểm số
+    //PhotonView view;
 
     void Start()
     {
-        Time.timeScale = 1;
+        //Time.timeScale = 1;
 
-        // Tìm và gán ScoreText và BXHText
-        scoreText = GameObject.Find("ScoreText")?.GetComponent<TMP_Text>();
-        BXHText = GameObject.Find("BXHText")?.GetComponent<TMP_Text>();
-        BXHText2 = GameObject.Find("BXHText2")?.GetComponent<TMP_Text>();
+        //// Tìm và gán ScoreText và BXHText
+        //scoreText = GameObject.Find("ScoreText")?.GetComponent<TMP_Text>();
+        //BXHText = GameObject.Find("BXHText")?.GetComponent<TMP_Text>();
+        //BXHText2 = GameObject.Find("BXHText2")?.GetComponent<TMP_Text>();
 
 
-        score = 0;
-        scoreText.text = "0";
+        //score = 0;
+        //scoreText.text = "0";
         myRigidbody = GetComponent<Rigidbody2D>();
         mySpriteRenderer = GetComponent<SpriteRenderer>();
-        view = GetComponent<PhotonView>();
+        //view = GetComponent<PhotonView>();
 
-        ResetPlayerScores();
+        //ResetPlayerScores();
 
-        // Thêm điểm của player vào từ điển
-        if (!playerScores.ContainsKey(view.Owner.ActorNumber))
-        {
-            playerScores.Add(view.Owner.ActorNumber, 0);
-        }
+        //// Thêm điểm của player vào từ điển
+        //if (!playerScores.ContainsKey(view.Owner.ActorNumber))
+        //{
+        //    playerScores.Add(view.Owner.ActorNumber, 0);
+        //}
 
-        // Nếu không tìm thấy, không báo lỗi, chỉ ghi nhật ký thông tin
-        if (scoreText == null)
-        {
-            Debug.Log("Không tìm thấy ScoreText trong cảnh. Vui lòng kiểm tra tên đối tượng.");
-        }
+        //// Nếu không tìm thấy, không báo lỗi, chỉ ghi nhật ký thông tin
+        //if (scoreText == null)
+        //{
+        //    Debug.Log("Không tìm thấy ScoreText trong cảnh. Vui lòng kiểm tra tên đối tượng.");
+        //}
 
-        if (BXHText == null)
-        {
-            Debug.Log("Không tìm thấy BXHText trong cảnh. Vui lòng kiểm tra tên đối tượng.");
-        }
-        else
-        {
-            BXHText.text = "Bảng xếp hạng:";
-        }
+        //if (BXHText == null)
+        //{
+        //    Debug.Log("Không tìm thấy BXHText trong cảnh. Vui lòng kiểm tra tên đối tượng.");
+        //}
+        //else
+        //{
+        //    BXHText.text = "Bảng xếp hạng:";
+        //}
 
-        if (BXHText2 == null)
-        {
-            Debug.Log("Không tìm thấy BXHText2 trong cảnh. Vui lòng kiểm tra tên đối tượng.");
-        }
+        //if (BXHText2 == null)
+        //{
+        //    Debug.Log("Không tìm thấy BXHText2 trong cảnh. Vui lòng kiểm tra tên đối tượng.");
+        //}
 
     }
 
-    private void FixedUpdate()
-    {
-        if (view.IsMine)
-        {
-            Flip();
-            //scoreText.text = score.ToString();
-        }
+    //private void FixedUpdate()
+    //{
+    //    if (view.IsMine)
+    //    {
+    //        Flip();
+    //        //scoreText.text = score.ToString();
+    //    }
 
-        if (PhotonNetwork.IsMasterClient)
-        {
-            if (Timer.TimeOver)
-            {
-                if (BXHText != null)
-                {
-                    BXHText2.text = BXHText.text;
-                    Time.timeScale = 0;
-                }
-            }
-        }
-    }
+    //    if (PhotonNetwork.IsMasterClient)
+    //    {
+    //        if (Timer.TimeOver)
+    //        {
+    //            if (BXHText != null)
+    //            {
+    //                BXHText2.text = BXHText.text;
+    //                Time.timeScale = 0;
+    //            }
+    //        }
+    //    }
+    //}
 
     void Update()
     {
+        Run();
+        Flip();
+        //if (view.IsMine)
+        //{
+        //    Run();
+        //}
 
-        if (view.IsMine)
-        {
-            Run();
-        }
-
-        if (scoreText != null)
-        {
-            scoreText.text = score.ToString();
-        }
+        //if (scoreText != null)
+        //{
+        //    scoreText.text = score.ToString();
+        //}
     }
 
     void OnMove(InputValue value)
@@ -107,68 +108,68 @@ public class PlayerTest : MonoBehaviourPunCallbacks
         moveInput = value.Get<Vector2>();
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Coin"))
-        {
-            if (view.IsMine)
-            {
-                score += 1;
+    //void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Coin"))
+    //    {
+    //        if (view.IsMine)
+    //        {
+    //            score += 1;
 
-                // Cập nhật điểm số trong từ điển
-                playerScores[view.Owner.ActorNumber] = score;
+    //            // Cập nhật điểm số trong từ điển
+    //            playerScores[view.Owner.ActorNumber] = score;
 
-                // Đồng bộ hóa điểm số với các player khác
-                photonView.RPC("UpdateScore", RpcTarget.All, view.Owner.ActorNumber, score);
-            }
-        }
-    }
+    //            // Đồng bộ hóa điểm số với các player khác
+    //            photonView.RPC("UpdateScore", RpcTarget.All, view.Owner.ActorNumber, score);
+    //        }
+    //    }
+    //}
 
-    [PunRPC]
-    void UpdateScore(int actorNumber, int newScore)
-    {
-        if (playerScores.ContainsKey(actorNumber))
-        {
-            playerScores[actorNumber] = newScore;
-        }
-        else
-        {
-            playerScores.Add(actorNumber, newScore);
-        }
+    //[PunRPC]
+    //void UpdateScore(int actorNumber, int newScore)
+    //{
+    //    if (playerScores.ContainsKey(actorNumber))
+    //    {
+    //        playerScores[actorNumber] = newScore;
+    //    }
+    //    else
+    //    {
+    //        playerScores.Add(actorNumber, newScore);
+    //    }
 
-        UpdateLeaderboard();
-    }
+    //    UpdateLeaderboard();
+    //}
 
-    void UpdateLeaderboard()
-    {
-        // Lấy danh sách các player được sắp xếp theo điểm số giảm dần
-        var sortedScores = new List<KeyValuePair<int, int>>(playerScores);
-        sortedScores.Sort((x, y) => y.Value.CompareTo(x.Value));
+    //void UpdateLeaderboard()
+    //{
+    //    // Lấy danh sách các player được sắp xếp theo điểm số giảm dần
+    //    var sortedScores = new List<KeyValuePair<int, int>>(playerScores);
+    //    sortedScores.Sort((x, y) => y.Value.CompareTo(x.Value));
 
-        // Cập nhật bảng xếp hạng
-        string leaderboardText = "BẢNG XẾP HẠNG:\n";
-        int i = 1;
-        foreach (var scoreEntry in sortedScores)
-        {
-            Player player = null;
-            if (PhotonNetwork.CurrentRoom != null)
-            {
-                PhotonNetwork.CurrentRoom.Players.TryGetValue(scoreEntry.Key, out player);
-            }
-            if (player != null)
-            {
-                leaderboardText += $"{i} - {player.NickName}: {scoreEntry.Value} điểm\n";
-                i++;
-            }
+    //    // Cập nhật bảng xếp hạng
+    //    string leaderboardText = "BẢNG XẾP HẠNG:\n";
+    //    int i = 1;
+    //    foreach (var scoreEntry in sortedScores)
+    //    {
+    //        Player player = null;
+    //        if (PhotonNetwork.CurrentRoom != null)
+    //        {
+    //            PhotonNetwork.CurrentRoom.Players.TryGetValue(scoreEntry.Key, out player);
+    //        }
+    //        if (player != null)
+    //        {
+    //            leaderboardText += $"{i} - {player.NickName}: {scoreEntry.Value} điểm\n";
+    //            i++;
+    //        }
 
-        }
+    //    }
 
-        // Chỉ cập nhật BXHText nếu không phải là null
-        if (BXHText != null)
-        {
-            BXHText.text = leaderboardText;
-        }
-    }
+    //    // Chỉ cập nhật BXHText nếu không phải là null
+    //    if (BXHText != null)
+    //    {
+    //        BXHText.text = leaderboardText;
+    //    }
+    //}
 
     void Flip()
     {
@@ -191,39 +192,39 @@ public class PlayerTest : MonoBehaviourPunCallbacks
         myRigidbody.velocity = playerHorizontal;
     }
 
-    public override void OnJoinedRoom()
-    {
-        base.OnJoinedRoom();
-        ResetPlayerScores();
-    }
+    //public override void OnJoinedRoom()
+    //{
+    //    base.OnJoinedRoom();
+    //    ResetPlayerScores();
+    //}
 
-    private void ResetPlayerScores()
-    {
-        if (PhotonNetwork.IsMasterClient)
-        {
-            // Xóa hết các giá trị đã lưu trong từ điển
-            playerScores.Clear();
+    //private void ResetPlayerScores()
+    //{
+    //    if (PhotonNetwork.IsMasterClient)
+    //    {
+    //        // Xóa hết các giá trị đã lưu trong từ điển
+    //        playerScores.Clear();
 
-            // Đồng bộ hóa điểm số với các player khác
-            photonView.RPC("SyncScores", RpcTarget.All, new Dictionary<int, int>());
-        }
-    }
+    //        // Đồng bộ hóa điểm số với các player khác
+    //        photonView.RPC("SyncScores", RpcTarget.All, new Dictionary<int, int>());
+    //    }
+    //}
 
-    [PunRPC]
-    void SyncScores(Dictionary<int, int> scores)
-    {
-        playerScores = scores;
-        UpdateLeaderboard();
-    }
+    //[PunRPC]
+    //void SyncScores(Dictionary<int, int> scores)
+    //{
+    //    playerScores = scores;
+    //    UpdateLeaderboard();
+    //}
 
-    // Gọi hàm này khi bắt đầu trò chơi
-    public void StartGame()
-    {
-        if (PhotonNetwork.IsMasterClient)
-        {
-            ResetPlayerScores();
-            PhotonNetwork.LoadLevel("GameScene"); // Thay đổi với tên scene của bạn
-        }
-    }
-    //BẢNG XẾP HẠNG
+    //// Gọi hàm này khi bắt đầu trò chơi
+    //public void StartGame()
+    //{
+    //    if (PhotonNetwork.IsMasterClient)
+    //    {
+    //        ResetPlayerScores();
+    //        PhotonNetwork.LoadLevel("GameScene"); // Thay đổi với tên scene của bạn
+    //    }
+    //}
+    ////BẢNG XẾP HẠNG
 }
