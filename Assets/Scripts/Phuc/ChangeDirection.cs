fc8d7e03e97112748c3250d7c9dffd8a278970af
Tạ Huỳnh Phúc <102749112+TaHuynhPhuc@users.noreply.github.com>
2024-07-07 22:47:01 +0700

    complete 99,96%

--- Assets/Scripts/Phuc/ChangeDirection.cs
+++ Assets/Scripts/Phuc/ChangeDirection.cs
@@ -3,14 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeDirection : MonoBehaviourPun
+public class ChangeDirection : MonoBehaviour
 {
     public GameObject player;
+    private SpriteRenderer sprite;
+    private PhotonView view;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        view = GetComponentInParent<PhotonView>();
+    }
 
     private void Update()
     {
-        if (photonView.IsMine)
+        if (view.IsMine)
         {
+            sprite.enabled = true;
             if (transform.position.x <= player.transform.position.x)
             {
                 transform.localScale = new Vector3(transform.localScale.x, -Mathf.Abs(transform.localScale.y), transform.localScale.z);
@@ -19,6 +28,10 @@ public class ChangeDirection : MonoBehaviourPun
             {
                 transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y), transform.localScale.z);
             }
+        }
+        else
+        {
+            sprite.enabled = false;
         }
     }
 }
