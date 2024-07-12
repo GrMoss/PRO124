c9476530a6a7e291bac3849564e64444d15204f3
Tạ Huỳnh Phúc <102749112+TaHuynhPhuc@users.noreply.github.com>
2024-07-12 07:08:49 +0700

    .

--- Assets/Scripts/Huy/UI/Time.cs
+++ Assets/Scripts/Huy/UI/Time.cs
@@ -51,9 +51,9 @@ public class Timer : MonoBehaviourPunCallbacks
                     TimeOver = true;
                     photonView.RPC("ShowPanelForAll", RpcTarget.All);
                 }
-                Debug.Log("Hết thời gian!");
+                //Debug.Log("Hết thời gian!");
             }
-            Debug.Log("LobbyManager.offLobby: " + LobbyManager.offLobby);
+            //Debug.Log("LobbyManager.offLobby: " + LobbyManager.offLobby);
         }
         if (!timerIsRunning)
         {
--- Assets/Scripts/Phuc/CookingController.cs
+++ Assets/Scripts/Phuc/CookingController.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,28 @@ using UnityEngine;
 public class CookingController : MonoBehaviour
 {
     public GameObject viewCooker;
+    private PhotonView view;
+    private Inventory_Manager inventory;
+
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
+        if(view.IsMine)
+        {
+            inventory = GameObject.Find("Inventory_Manager").GetComponent<Inventory_Manager>();
+            if(inventory != null)
+            {
+                Debug.Log("oke");
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             viewCooker.SetActive(true);
+            inventory.ShowAllInventoryData();
         }
     }
 
