fbc42ccde8bf638d7e4bf854614ab5478cf4ad9a
GrMoss <126696013+GrMoss@users.noreply.github.com>
2024-07-12 19:01:14 +0700

    chỉnh ani lobby

--- Assets/Scripts/Huy/Inventory/Inventory_Manager.cs
+++ Assets/Scripts/Huy/Inventory/Inventory_Manager.cs
@@ -11,44 +11,90 @@ using System.Linq;
 
 public class Inventory_Manager : MonoBehaviour
 {
-    public static List<InventoryData> lisInventoryDatas = new List<InventoryData>
-    {
-        new InventoryData(0, "Null" , 0),
-        new InventoryData(1, "Chilli", 0),
-        new InventoryData(2, "Carrot", 0),
-        new InventoryData(3, "Cucumber", 0),
-        new InventoryData(4, "Egg", 0),
-        new InventoryData(5, "Omelet", 0),
-        new InventoryData(6, "Bread", 0),
-        new InventoryData(7, "ChinSo", 0)
+    PhotonView view;
+    List<InventoryData> lisInventoryDatas = new List<InventoryData>();
 
-    };
+    private void Start()
+    {
+        KhoiTao();
+    }
+
+    public void KhoiTao()
+    {
+        if (view.IsMine)
+        {
+            lisInventoryDatas.Add(new InventoryData(0, "Null", 0));
+            lisInventoryDatas.Add(new InventoryData(1, "Chilli", 0));
+            lisInventoryDatas.Add(new InventoryData(3, "Cucumber", 0));
+            lisInventoryDatas.Add(new InventoryData(4, "Egg", 0));
+            lisInventoryDatas.Add(new InventoryData(5, "Omelet", 0));
+            lisInventoryDatas.Add(new InventoryData(6, "Bread", 0));
+            lisInventoryDatas.Add(new InventoryData(7, "ChinSo", 0));
+        }
+
+    }
 
     public void AddItemInList(int id, int quantity)
     {
-        var item = lisInventoryDatas.SingleOrDefault(x => x.ItemID == id);
-        if (item != null) item.QuantityItem += quantity;
+        if (view.IsMine)
+        {
+            var item = lisInventoryDatas.SingleOrDefault(x => x.ItemID == id);
+            if (item != null) item.QuantityItem += quantity;
+            // Tùy chọn, đồng bộ hóa thay đổi này với các client khác
+            //view.RPC("DongBoThemVatPham", RpcTarget.Others, id, soLuong);
+        }
     }
 
     public void QuitItemInList(int id, int quantity)
     {
-        var item = lisInventoryDatas.SingleOrDefault(x => x.ItemID == id);
-        if (item != null) item.QuantityItem -= quantity;
+        if (view.IsMine)
+        {
+            var item = lisInventoryDatas.SingleOrDefault(x => x.ItemID == id);
+            if (item != null) item.QuantityItem -= quantity;
+            // Tùy chọn, đồng bộ hóa thay đổi này với các client khác
+            // view.RPC("DongBoXoaVatPham", RpcTarget.Others, id, soLuong);
+        }
+    
     }
 
     public int GetQuatityItem(int id)
     {
-        var item = lisInventoryDatas.SingleOrDefault(x => x.ItemID == id);
-        
-        return item.QuantityItem;
+        if (view.IsMine)
+        {
+            var item = lisInventoryDatas.SingleOrDefault(x => x.ItemID == id);
+
+            return item.QuantityItem;
+        }
+        return 0; 
     }
 
     public void ShowAllInventoryData()
     {
-        var showALL = lisInventoryDatas.ToList();
-        foreach (var item in showALL)
+        if (view.IsMine)
         {
-            Debug.Log($"ID: {item.ItemID} | Name: {item.ItemName} | Quality: {item.QuantityItem}" );
+            var showALL = lisInventoryDatas.ToList();
+            foreach (var item in showALL)
+            {
+                Debug.Log($"ID: {item.ItemID} | Name: {item.ItemName} | Quality: {item.QuantityItem}");
+            }
         }
+    
     }
+
+    // Ví dụ về các phương thức RPC để đồng bộ hóa (tùy chọn)
+    /*
+    [PunRPC]
+    private void DongBoThemVatPham(int id, int soLuong)
+    {
+        var vatPham = danhSachDuLieuKho.SingleOrDefault(x => x.ItemID == id);
+        if (vatPham != null) vatPham.QuantityItem += soLuong;
+    }
+
+    [PunRPC]
+    private void DongBoXoaVatPham(int id, int soLuong)
+    {
+        var vatPham = danhSachDuLieuKho.SingleOrDefault(x => x.ItemID == id);
+        if (vatPham != null) vatPham.QuantityItem -= soLuong;
+    }
+    */
 }
