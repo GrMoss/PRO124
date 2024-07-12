31bdd14000d207a94969feaa1e70b5d53c704874
TV-Trong <trongtvps38376@fpt.edu.vn>
2024-07-12 10:29:43 +0700

    Thêm âm thanh cho player

--- Assets/Scripts/Trong/AudioManager.cs
+++ Assets/Scripts/Trong/AudioManager.cs
@@ -21,6 +21,14 @@ public class AudioManager : MonoBehaviour
     public AudioClip buttonPressed2;
     public AudioClip logo;
 
+    [Header("Player Effects")]
+    public AudioClip fainted1;
+    public AudioClip fainted2;
+    public AudioClip fainted3;
+    public AudioClip hurt1;
+    public AudioClip hurt2;
+    public AudioClip hurt3;
+
     private static AudioManager instance;
     private void Awake()
     {
