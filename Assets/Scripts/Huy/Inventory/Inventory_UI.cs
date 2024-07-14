1a52e19ec4c35330eec754c6fb689d36913bbe88
GrMoss <126696013+GrMoss@users.noreply.github.com>
2024-07-14 16:16:19 +0700

    fix UI

--- Assets/Scripts/Huy/Inventory/Inventory_UI.cs
+++ Assets/Scripts/Huy/Inventory/Inventory_UI.cs
@@ -1,8 +1,10 @@
+
 //using System.Collections;
 //using System.Collections.Generic;
 //using UnityEngine;
 //using UnityEngine.UI;
 //using Photon.Pun;
+//using TMPro;
 
 //public class Inventory_UI : MonoBehaviourPun
 //{
@@ -31,7 +33,6 @@
 //            if (inventory_Manager != null)
 //            {
 //                Debug.Log("Đã gán Inventory_Manager cho Inventory_UI.");
-//                UpdateInventoryUI();
 //            }
 //            else
 //            {
@@ -46,12 +47,18 @@
 //        {
 //            if (Input.GetKeyDown(KeyCode.Tab))
 //            {
-//                ToggleInventoryUI();
+//                inventoryUI.SetActive(!inventoryUI.activeSelf);
+//                UpdateInventoryUI();
+
+//                //if (inventoryUI.activeSelf)
+//                //{
+//                //    UpdateInventoryUI();
+//                //}
 //            }
 //        }
 //    }
 
-//    private void UpdateInventoryUI()
+//    public void UpdateInventoryUI()
 //    {
 //        if (photonView.IsMine && inventory_Manager != null)
 //        {
@@ -64,23 +71,46 @@
 //            // Duyệt qua danh sách các item trong kho và hiển thị trên UI
 //            foreach (var item in inventory_Manager.GetInventoryItems())
 //            {
-//                GameObject slot = Instantiate(inventorySlotPrefab, inventoryPanel);
-//                slot.transform.Find("ItemName").GetComponent<Text>().text = item.ItemName;
-//                slot.transform.Find("Quantity").GetComponent<Text>().text = $"Quantity: {item.QuantityItem}";
-//                slot.transform.Find("ItemImage").GetComponent<Image>().sprite = item.ImageItem;
+//                if (item.QuantityItem > 0)
+//                {
+//                    Debug.Log($"Đang tạo slot cho item: {item.ItemName}, Quantity: {item.QuantityItem}");
+//                    GameObject slot = Instantiate(inventorySlotPrefab, inventoryPanel);
+//                    //TMP_Text itemNameText = slot.transform.Find("ItemName").GetComponent<TMP_Text>();
+//                    TMP_Text quantityText = slot.transform.Find("Quantity").GetComponent<TMP_Text>();
+//                    Image itemImage = slot.transform.Find("ItemImage").GetComponent<Image>();
+
+//                    //if (itemNameText != null)
+//                    //{
+//                    //    itemNameText.text = item.ItemName;
+//                    //}
+//                    //else
+//                    //{
+//                    //    Debug.LogError("Không tìm thấy thành phần TMP_Text cho ItemName.");
+//                    //}
+
+//                    if (quantityText != null)
+//                    {
+//                        quantityText.text = item.QuantityItem.ToString();
+//                    }
+//                    else
+//                    {
+//                        Debug.LogError("Không tìm thấy thành phần TMP_Text cho Quantity.");
+//                    }
+
+//                    if (itemImage != null)
+//                    {
+//                        itemImage.sprite = item.ImageItem;
+//                    }
+//                    else
+//                    {
+
+//                    }
+//                    {
+//                        Debug.LogError("Không tìm thấy thành phần Image cho ItemImage.");
+//                    }
+//                }
 //            }
 //        }
-//    }
-
-//    private void ToggleInventoryUI()
-//    {
-//        inventoryUI.SetActive(!inventoryUI.activeSelf);
-
-//        if (inventoryUI.activeSelf)
-//        {
-//            UpdateInventoryUI();
-//        }
-
 //    }
 //}
 using System.Collections;
@@ -95,29 +125,29 @@ public class Inventory_UI : MonoBehaviourPun
     public GameObject inventorySlotPrefab; // Prefab của slot trong kho
     public Transform inventoryPanel; // Panel chứa các slot
     public GameObject inventoryUI;
-
+    public static bool inventoryUIOn = false;
     private Inventory_Manager inventory_Manager; // Tham chiếu đến Inventory_Manager
-    public static bool inventoryUIOn;
+    private ItemPrefab itemPrefab;
 
     private void Start()
     {
         inventoryUI.SetActive(false);
         inventoryUIOn = false;
+        // Tìm Inventory_Manager trên đối tượng cha trước
+        inventory_Manager = GetComponentInParent<Inventory_Manager>();
+        itemPrefab = FindObjectOfType<ItemPrefab>();
+
+        // Nếu không tìm thấy, dùng FindObjectOfType để tìm trong toàn bộ scene
+        if (inventory_Manager == null)
+        {
+            inventory_Manager = FindObjectOfType<Inventory_Manager>();
+        }
+
         if (photonView.IsMine)
         {
-            // Tìm Inventory_Manager trên đối tượng cha trước
-            inventory_Manager = GetComponentInParent<Inventory_Manager>();
-
-            // Nếu không tìm thấy, dùng FindObjectOfType để tìm trong toàn bộ scene
-            if (inventory_Manager == null)
-            {
-                inventory_Manager = FindObjectOfType<Inventory_Manager>();
-            }
-
             if (inventory_Manager != null)
             {
                 Debug.Log("Đã gán Inventory_Manager cho Inventory_UI.");
-                UpdateInventoryUI();
             }
             else
             {
@@ -132,12 +162,14 @@ public class Inventory_UI : MonoBehaviourPun
         {
             if (Input.GetKeyDown(KeyCode.Tab))
             {
-                ToggleInventoryUI();
+                inventoryUI.SetActive(!inventoryUI.activeSelf);
+                inventoryUIOn = inventoryUI.activeSelf;
+                UpdateInventoryUI();
             }
         }
     }
 
-    private void UpdateInventoryUI()
+    public void UpdateInventoryUI()
     {
         if (photonView.IsMine && inventory_Manager != null)
         {
@@ -150,49 +182,45 @@ public class Inventory_UI : MonoBehaviourPun
             // Duyệt qua danh sách các item trong kho và hiển thị trên UI
             foreach (var item in inventory_Manager.GetInventoryItems())
             {
-                Debug.Log($"Đang tạo slot cho item: {item.ItemName}, Quantity: {item.QuantityItem}");
-                GameObject slot = Instantiate(inventorySlotPrefab, inventoryPanel);
-                TMP_Text itemNameText = slot.transform.Find("ItemName").GetComponent<TMP_Text>();
-                TMP_Text quantityText = slot.transform.Find("Quantity").GetComponent<TMP_Text>();
-                Image itemImage = slot.transform.Find("ItemImage").GetComponent<Image>();
+                if (item.QuantityItem > 0)
+                {
+                    Debug.Log($"Đang tạo slot cho item: {item.ItemName}, Quantity: {item.QuantityItem}");
+                    GameObject slot = Instantiate(inventorySlotPrefab, inventoryPanel);
 
-                if (itemNameText != null)
-                {
-                    itemNameText.text = item.ItemName;
-                }
-                else
-                {
-                    Debug.LogError("Không tìm thấy thành phần TMP_Text cho ItemName.");
-                }
+                    // Lấy component ItemPrefab từ slot mới tạo
+                    ItemPrefab itemPrefabComponent = slot.GetComponent<ItemPrefab>();
 
-                if (quantityText != null)
-                {
-                    quantityText.text = $"Quantity: {item.QuantityItem}";
-                }
-                else
-                {
-                    Debug.LogError("Không tìm thấy thành phần TMP_Text cho Quantity.");
-                }
+                    if (itemPrefabComponent != null)
+                    {
+                        itemPrefabComponent.itemID = item.ItemID; 
+                    }
+                    else
+                    {
+                        Debug.LogError("Không tìm thấy thành phần ItemPrefab trên slot.");
+                    }
 
-                if (itemImage != null)
-                {
-                    itemImage.sprite = item.ImageItem;
-                }
-                else
-                {
-                    Debug.LogError("Không tìm thấy thành phần Image cho ItemImage.");
+                    TMP_Text quantityText = slot.transform.Find("Quantity").GetComponent<TMP_Text>();
+                    Image itemImage = slot.transform.Find("ItemImage").GetComponent<Image>();
+
+                    if (quantityText != null)
+                    {
+                        quantityText.text = item.QuantityItem.ToString();
+                    }
+                    else
+                    {
+                        Debug.LogError("Không tìm thấy thành phần TMP_Text cho Quantity.");
+                    }
+
+                    if (itemImage != null)
+                    {
+                        itemImage.sprite = item.ImageItem;
+                    }
+                    else
+                    {
+                        Debug.LogError("Không tìm thấy thành phần Image cho ItemImage.");
+                    }
                 }
             }
-        }
-    }
-
-    private void ToggleInventoryUI()
-    {
-        inventoryUI.SetActive(!inventoryUI.activeSelf);
-
-        if (inventoryUI.activeSelf)
-        {
-            UpdateInventoryUI();
         }
     }
 }
--- Assets/Scripts/Huy/Inventory/ItemPrefab.cs
+++ Assets/Scripts/Huy/Inventory/ItemPrefab.cs
@@ -1,22 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Photon.Pun;
 
-public class GetItem : MonoBehaviourPun
+public class ItemPrefab : MonoBehaviourPun
 {
-    public int[] itemID;
-    public int quantity;
- 
+    public int itemID;
     private Inventory_Manager inventory_Manager;
     private Inventory_UI inventory_UI;
+    private Shooting shooting;
 
     private void Start()
     {
-        // Tìm Inventory_Manager trên đối tượng cha trước
-        inventory_Manager = GetComponentInParent<Inventory_Manager>();
         inventory_UI = FindObjectOfType<Inventory_UI>();
+        inventory_Manager = FindObjectOfType<Inventory_Manager>();
+        shooting = FindObjectOfType<Shooting>();
 
         if (photonView.IsMine)
         {
+
             if (inventory_Manager != null)
             {
                 Debug.Log("Đã gán Inventory_Manager cho ItemPrefab.");
@@ -37,22 +40,12 @@ public class GetItem : MonoBehaviourPun
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void PressButton()
     {
         if (photonView.IsMine)
         {
-            for (int i = 0; i < itemID.Length; i++)
-            {
-                if (collision.CompareTag(i.ToString()))
-                {
-                    // Thêm vật phẩm vào túi của người chơi hiện tại
-                    inventory_Manager.AddItemInList(itemID[i], quantity);
-                    PhotonNetwork.Destroy(collision.gameObject); // Xóa đối tượng va chạm
-                    inventory_UI.UpdateInventoryUI();
-                    break; // Thoát khỏi vòng lặp sau khi thêm vật phẩm
-                }
-            }
+            Debug.Log("pressTest1 ID: " + itemID);
+            shooting.indexChooseFood = itemID;
         }
     }
-
 }
