3795290246941817fc9055f2cc0084aec8487b29
GrMoss <126696013+GrMoss@users.noreply.github.com>
2024-07-17 02:00:41 +0700

    Reapply "fix chọn bắng 1.1"
    This reverts commit 8cb488ba1bc9053c8ff3483585c43955a71c22c0.

--- Assets/Scripts/Huy/Inventory/ItemSlot.cs
+++ Assets/Scripts/Huy/Inventory/ItemSlot.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.EventSystems;
 using Photon.Pun;
 
-public class ItemSlot : MonoBehaviourPun
+public class ItemSlot : MonoBehaviourPun, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public int itemID;
     public bool indexShooting;
     private Inventory_Manager inventory_Manager;
-    private Inventory_UI inventory_UI;
+    private Inventory_Bar inventory_Bar;
+
+    public bool selectedButton = false;
+
+    public delegate void SelectionChangedHandler(bool isSelected);
+    public event SelectionChangedHandler OnSelectionChanged;
 
     public delegate void ItemSelectedHandler(int itemID);
     public event ItemSelectedHandler OnItemSelected;
 
     private void Start()
     {
-        inventory_UI = FindObjectOfType<Inventory_UI>();
         inventory_Manager = FindObjectOfType<Inventory_Manager>();
+        inventory_Bar = FindObjectOfType<Inventory_Bar>();
 
         if (photonView.IsMine)
         {
@@ -30,23 +35,43 @@ public class ItemSlot : MonoBehaviourPun
                 Debug.LogError("Không tìm thấy Inventory_Manager trên đối tượng này hoặc trong scene.");
             }
 
-            if (inventory_UI != null)
+            if (inventory_Bar != null)
             {
-                Debug.Log("Đã gán inventory_UI cho ItemPrefab.");
+                Debug.Log("Đã gán Inventory_UI cho ItemPrefab.");
             }
             else
             {
-                Debug.LogError("Không tìm thấy inventory_UI trên đối tượng này hoặc trong scene.");
+                Debug.LogError("Không tìm thấy Inventory_UI trên đối tượng này hoặc trong scene.");
             }
         }
     }
 
-    public void PressButton()
+    public void OnPointerEnter(PointerEventData eventData)
     {
         if (photonView.IsMine)
         {
+            selectedButton = true;
+            OnSelectionChanged?.Invoke(selectedButton);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (photonView.IsMine)
+        {
+            selectedButton = false;
+            OnSelectionChanged?.Invoke(selectedButton);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (photonView.IsMine)
+        {
+            selectedButton = true;
             indexShooting = true;
-            OnItemSelected?.Invoke(itemID);
+            inventory_Bar.indexShooting = itemID;
+            OnSelectionChanged?.Invoke(selectedButton);
         }
     }
 }
