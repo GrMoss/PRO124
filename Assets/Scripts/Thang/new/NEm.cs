5d25762313b1760e1afde9e369ab36eaac2659a7
bapcao <caorua04@gmail.com>
2024-07-19 07:35:43 +0700

    HTP
    bắn, panel hướng dẫn

---
+++ Assets/Scripts/Thang/new/NEm.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NEm : MonoBehaviour
+{
+    public GameObject objectToThrow;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Kiểm tra khi click chuột trái
+        {
+            ThrowObject();
+        }
+    }
+
+    void ThrowObject()
+    {
+        // Tạo một phiên bản của GameObject và ném ra
+        GameObject thrownObject = Instantiate(objectToThrow, transform.position, Quaternion.identity);
+
+        // Đặt hướng và tốc độ cho việc ném ra
+        Rigidbody rb = thrownObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(transform.forward * 500f); // Thay đổi hướng và tốc độ ném tại đây
+        }
+    }
+}
