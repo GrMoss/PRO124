1fc4f4c9e847d8e7ad50918daa91679237a0a02f
GrMoss <126696013+GrMoss@users.noreply.github.com>
2024-07-19 17:45:54 +0700

    fix inventory bar 1.2

--- Assets/Scripts/Huy/Inventory/GetItem.cs
+++ Assets/Scripts/Huy/Inventory/GetItem.cs
@@ -7,30 +7,14 @@ public class GetItem : MonoBehaviourPun
     public int quantity;
  
     private Inventory_Manager inventory_Manager;
-    //private Inventory_Bar inventory_Bar;
+    private Inventory_Bar inventory_Bar;
 
     private void Start()
     {
         // Tìm Inventory_Manager trên đối tượng cha trước
         inventory_Manager = GetComponentInParent<Inventory_Manager>();
-        //inventory_Bar = FindObjectOfType<Inventory_Bar>();
+        inventory_Bar = FindObjectOfType<Inventory_Bar>();
     }
-
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (photonView.IsMine)
-    //    {
-    //        for (int i = 0; i < itemID.Length; i++)
-    //        {
-    //            if (collision.gameObject.CompareTag(i.ToString()))
-    //            {
-    //                // Thêm vật phẩm vào túi của người chơi hiện tại
-    //                inventory_Manager.AddItemInList(itemID[i], quantity);
-    //                break; // Thoát khỏi vòng lặp sau khi thêm vật phẩm
-    //            }
-    //        }
-    //    }
-    //}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -43,6 +27,7 @@ public class GetItem : MonoBehaviourPun
                 {
                     // Thêm vật phẩm vào túi của người chơi hiện tại
                     inventory_Manager.AddItemInList(itemID[i], quantity);
+                    inventory_Bar.UpdateInventoryBar();
                     break; // Thoát khỏi vòng lặp sau khi thêm vật phẩm
                 }
             }
