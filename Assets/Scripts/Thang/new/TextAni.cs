a723f507f27abf966391c2934cfcb1047b6fd8f2
bapcao <caorua04@gmail.com>
2024-07-22 16:08:47 +0700

    HTP
    Chuyển vị trí panel.
    Tốc độ chữ

--- Assets/Scripts/Thang/new/TextAni.cs
+++ Assets/Scripts/Thang/new/TextAni.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -9,9 +9,10 @@ public class TextAni : MonoBehaviour
 
     public string[] stringArray;
 
-    [SerializeField] float timeBtwnChars;
-    [SerializeField] float timeBtwnWords;
+    [SerializeField] float timeBtwnChars; // Thời gian giữa mỗi ký tự
+    [SerializeField] float timeBtwnWords; // Thời gian giữa mỗi từ
     int i = 0;
+
     void Start()
     {
         EndCheck();
@@ -19,24 +20,25 @@ public class TextAni : MonoBehaviour
 
     public void EndCheck()
     {
-        if( i <= stringArray.Length - 1)
+        if (i <= stringArray.Length - 1)
         {
             _textMeshPro.text = stringArray[i];
             StartCoroutine(TextVisible());
         }
     }
+
     private IEnumerator TextVisible()
     {
         _textMeshPro.ForceMeshUpdate();
-        int totalVIsitbleCharacter = _textMeshPro.textInfo.characterCount;
+        int totalVisibleCharacter = _textMeshPro.textInfo.characterCount;
         int counter = 0;
 
         while (true)
         {
-            int visibleCount = counter % (totalVIsitbleCharacter + 1);
+            int visibleCount = counter % (totalVisibleCharacter + 1);
             _textMeshPro.maxVisibleCharacters = visibleCount;
 
-            if (visibleCount >= totalVIsitbleCharacter)
+            if (visibleCount >= totalVisibleCharacter)
             {
                 i += 1;
                 Invoke("EndCheck", timeBtwnWords);
@@ -44,7 +46,7 @@ public class TextAni : MonoBehaviour
             }
 
             counter += 1;
-            yield return new WaitForSeconds(timeBtwnChars);
+            yield return new WaitForSeconds(timeBtwnChars / 10); // Tăng tốc độ hiệu ứng chữ bằng cách chia độ trễ
         }
 
     }
